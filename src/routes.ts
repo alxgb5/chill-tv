917d3e54b995dc30f0cf634f8b3dfd88c817f457
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-02-02 17:39:56 +0100

    routes list

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+export const RoutesList = {
+    Index: '',
+    Login: 'login',
+    Register: 'register',
+    Home: 'home',
+    Dashboard: 'dasboard',
+    NotFound: 'not-found',
+
+    // ADMINISTRATION ROUTES
+    AdminUsers: 'admin/users',
+    AdminLogs: 'admin/logs',
+};
