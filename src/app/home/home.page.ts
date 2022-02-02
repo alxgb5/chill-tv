1541686817bc2bc978fc0d913e434227d0337a69
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-02-03 00:32:56 +0100

    home improve

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,7 +8,17 @@ import { BaseComponent } from '../../base/base.component';
     encapsulation: ViewEncapsulation.None,
 })
 export class HomePage extends BaseComponent {
+    backgroundBanner: string = '/assets/img/breaking-bad.png';
     constructor() {
         super();
+    }
+
+    setBanner(banner: number) {
+        if (banner === 1)
+            this.backgroundBanner = '/assets/img/breaking-bad.png';
+        if (banner === 2)
+            this.backgroundBanner = '/assets/img/spiderman.png';
+        if (banner === 3)
+            this.backgroundBanner = '/assets/img/naruto.png';
     }
 }
