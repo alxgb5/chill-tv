b6dd363200f0e613d3b27b68022f4c7d498e4661
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-02-02 17:41:27 +0100

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { BaseComponent } from '../base/base.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent extends BaseComponent {
   title = 'chilltv';
+  constructor(
+  ) {
+    super();
+  }
 }
