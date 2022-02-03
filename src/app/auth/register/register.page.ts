e085566973dd7f51eb2d63fd3d33d3cea1581d21
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-02-03 17:37:32 +0100

    register improve

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { BaseComponent } from '../../../base/base.component';
+import { DialogService } from '../../../base/dialog.service';
 import { GenresDto } from '../../../models/genres-dto';
 import { PlatformDto } from '../../../models/platform-dto';
 import { UserDto } from '../../../models/user-dto';
@@ -9,7 +10,7 @@ interface PlateformWrapper {
     selected: boolean;
 }
 interface GenresWrapper {
-    platform: GenresDto;
+    genre: GenresDto;
     selected: boolean;
 }
 @Component({
@@ -23,19 +24,24 @@ export class RegisterPage extends BaseComponent {
     user: UserDto = {} as UserDto;
     selectedGenres: GenresWrapper[] = [];
     activeStepper: number = 1;
-    constructor() {
+    confirmPassword: string = '';
+    constructor(
+        private dialog: DialogService,
+    ) {
         super();
         this.initData();
     }
 
     switchStep(step: number) {
-        if (step === 1)
+        if (step === 1) {
             this.activeStepper = 1;
+        }
         if (step === 2)
             this.activeStepper = 2;
         if (step === 3)
             this.activeStepper = 3;
     }
+
     nextStep() {
         if (this.activeStepper === 3)
             return;
@@ -44,6 +50,26 @@ export class RegisterPage extends BaseComponent {
 
     selectPlatform(wrapper: PlateformWrapper) {
         wrapper.selected = !wrapper.selected;
+    }
+    selectGenre(wrapper: GenresWrapper) {
+        wrapper.selected = !wrapper.selected;
+    }
+
+    register() {
+        if (!this.user.email || !this.user.password || !this.confirmPassword || !this.user.username) {
+            // this.dialog.showDialog({ text: "Vous devez compléter toutes les informations requises", header: 'de', cancelLabel: "Confirmer", okLabel: 'ok' });
+            return;
+        }
+        const platformsSelected = this.selectedPlateform.filter(x => x.selected === true);
+        if (!platformsSelected.length) {
+            //  dialog need to fill in
+            return;
+        }
+        const genresSelected = this.selectedGenres.filter(x => x.selected === true);
+        if (!genresSelected.length) {
+            //  dialog need to fill in
+            return;
+        }
     }
 
     initData() {
@@ -109,6 +135,92 @@ export class RegisterPage extends BaseComponent {
                     name: 'ADN',
                     imgUrl: '/assets/img/adn.png',
                     code: 'adn',
+                },
+                selected: false,
+            },
+        ];
+        this.selectedGenres = [
+            {
+                genre: {
+                    code: 'action',
+                    name: 'Action',
+                },
+                selected: false,
+            },
+            {
+                genre: {
+                    code: 'anime',
+                    name: 'Anime',
+                },
+                selected: false,
+            },
+            {
+                genre: {
+                    code: 'horreur',
+                    name: 'Horreur',
+                },
+                selected: false,
+            },
+            {
+                genre: {
+                    code: 'fantastique',
+                    name: 'Fantastique',
+                },
+                selected: false,
+            },
+            {
+                genre: {
+                    code: 'sciencefiction',
+                    name: 'Science-fiction',
+                },
+                selected: false,
+            },
+            {
+                genre: {
+                    code: 'humour',
+                    name: 'Humour',
+                },
+                selected: false,
+            },
+            {
+                genre: {
+                    code: 'comédie',
+                    name: 'Comédie',
+                },
+                selected: false,
+            },
+            {
+                genre: {
+                    code: 'magie',
+                    name: 'Magie',
+                },
+                selected: false,
+            },
+            {
+                genre: {
+                    code: 'aventure',
+                    name: 'Aventure',
+                },
+                selected: false,
+            },
+            {
+                genre: {
+                    code: 'futuriste',
+                    name: 'Futuriste',
+                },
+                selected: false,
+            },
+            {
+                genre: {
+                    code: 'espace',
+                    name: 'Espace',
+                },
+                selected: false,
+            },
+            {
+                genre: {
+                    code: 'drame',
+                    name: 'Drame',
                 },
                 selected: false,
             },
