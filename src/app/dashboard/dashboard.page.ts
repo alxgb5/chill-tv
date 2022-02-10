ac58297236453a284dabf6139146eb757778f4b1
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-02-10 23:38:16 +0100

    logout btn

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { getAuth, signOut } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 import { BaseComponent } from '../../base/base.component';
 
 @Component({
@@ -7,7 +9,21 @@ import { BaseComponent } from '../../base/base.component';
     encapsulation: ViewEncapsulation.None,
 })
 export class DashboardPage extends BaseComponent {
-    constructor() {
+    constructor(
+        private router: Router,
+    ) {
         super();
+    }
+
+    async logOut() {
+        try {
+            const auth = getAuth();
+            console.log("🚀 ~ DashboardPage ~ logOut ~ auth", auth);
+            const signout = await signOut(auth);
+            console.log("🚀 ~ DashboardPage ~ logOut ~ signout", signout);
+            this.router.navigateByUrl('/' + this.RoutesList.Login);
+        } catch (error) {
+            console.log("Error : ", error)
+        }
     }
 }
